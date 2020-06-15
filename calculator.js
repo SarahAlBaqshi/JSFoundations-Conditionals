48792df3d149ff276684d3f7e7b2cf1517fc33cd
Sarah <S00038939@auk.edu.kw>
2020-06-15 19:52:38 +0300

    Conditionals task completed.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,15 @@
 // Define the numbers and the operation here
+const x = 5;
+const y = 3;
+const operation = "*";
 
 // Write if-statements here
+if (operation === "+") {
+  console.log(x + y);
+} else if (operation === "-") {
+  console.log(x - y);
+} else if (operation === "*") {
+  console.log(x * y);
+} else if (operation === "/") {
+  console.log(x / y);
+}
